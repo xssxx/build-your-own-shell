664dbee72897eb64500d2bf8def212c8343e9266
xssxx <koonx6520@gmail.com>
2026-06-02 20:32:07 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -12,6 +12,38 @@ import (
 
 var builtin []string = []string{"echo", "exit", "type", "pwd", "cd"}
 
+func parseArgs(input string) []string {
+	var args []string
+	var curr strings.Builder
+	inSingle := false
+	inDouble := false
+
+	for i := 0; i < len(input); i++ {
+		ch := input[i]
+
+		switch {
+		case ch == '\'' && !inDouble:
+			inSingle = !inSingle
+		case ch == '"' && !inSingle:
+			inDouble = !inDouble
+		case (ch == ' ' || ch == '\t') && !inSingle && !inDouble:
+			if curr.Len() > 0 {
+				args = append(args, curr.String())
+				curr.Reset()
+			}
+		default:
+			curr.WriteByte(ch)
+		}
+
+	}
+
+	if curr.Len() > 0 {
+		args = append(args, curr.String())
+	}
+
+	return args
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
@@ -19,7 +51,7 @@ func main() {
 		fmt.Print("$ ")
 		cmd, err := reader.ReadString('\n')
 		cmd = strings.TrimSpace(cmd)
-		fields := strings.Fields(cmd)
+		fields := parseArgs(cmd)
 
 		if len(fields[0]) == 0 {
 			os.Exit(0)
@@ -30,16 +62,17 @@ func main() {
 			os.Exit(1)
 		}
 
-		if strings.HasPrefix(fields[0], "exit") {
+		commandName := fields[0]
+		arguments := fields[1:]
+
+		if strings.HasPrefix(commandName, "exit") {
 			os.Exit(0)
-		} else if strings.HasPrefix(fields[0], "echo") {
-			fmt.Println(cmd[5:])
-		} else if strings.HasPrefix(fields[0], "type") {
-			handleType(strings.TrimSpace(fields[1]))
-		} else if slices.Contains(builtin, strings.TrimSpace(fields[0])) {
-			execBuiltin(strings.TrimSpace(fields[0]), fields[1:])
-		} else if _, err := exec.LookPath(fields[0]); err == nil {
-			cmd := exec.Command(fields[0], fields[1:]...)
+		} else if strings.HasPrefix(commandName, "type") {
+			handleType(fields[1])
+		} else if slices.Contains(builtin, commandName) {
+			execBuiltin(commandName, arguments)
+		} else if _, err := exec.LookPath(commandName); err == nil {
+			cmd := exec.Command(commandName, arguments...)
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			cmd.Run()
@@ -92,5 +125,12 @@ func execBuiltin(cmd string, args []string) {
 			fmt.Println("cd: " + path + ": No such file or directory")
 			return
 		}
+	case "echo":
+		for i := range args {
+			args[i] = strings.TrimSpace(args[i])
+		}
+		fmt.Println(strings.Join(args, " "))
+	default:
+		fmt.Println(cmd + ": command not found")
 	}
 }
