e9b2ec428d97ee368604c67c9bfbe287b156bee0
xssxx <koonx6520@gmail.com>
2026-06-02 19:28:13 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,42 +4,44 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"slices"
 	"strings"
 )
 
-var _ = fmt.Print
-
 func main() {
-	readInput()
-}
+	builtin := []string{"echo", "exit", "type"}
 
-var builtin []string = []string{"echo", "exit", "type"}
-
-func readInput() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
-		command, err := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
 
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		command = strings.TrimSpace(command)
-		if command == "exit" {
-			break
-		} else if strings.HasPrefix(command, "echo ") {
-			fmt.Println(command[5:])
-		} else if strings.HasPrefix(command, "type ") {
-			cmd := strings.TrimSpace(command[5:])
-			if slices.Contains(builtin, cmd) {
-				fmt.Println(cmd + " is a shell builtin")
+
+		input = strings.TrimSpace(input)
+		inputParts := strings.Split(input, " ")
+		cmd := inputParts[0]
+		args := inputParts[1:]
+
+		switch cmd {
+		case "exit":
+			os.Exit(0)
+		case "echo":
+			fmt.Println(strings.Join(args, " "))
+		case "type":
+			if slices.Contains(builtin, args[0]) {
+				fmt.Println(args[0] + " is a shell builtin")
+			} else if path, _ := exec.LookPath(args[0]); path != "" {
+				fmt.Println(args[0] + " is " + path)
 			} else {
-				fmt.Println(cmd + ": not found")
+				fmt.Println(args[0] + ": not found")
 			}
-		} else {
-			fmt.Println(command + ": command not found")
+		default:
+			fmt.Println(cmd + ": command not found")
 		}
 	}
 }
